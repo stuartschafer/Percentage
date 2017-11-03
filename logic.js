3f56986780bdbc0c4fa871f94efa352914590318
Stuart Schafer <stuart_schafer@hotmail.com>
2017-11-03 18:31:06 -0400

    Started basic design and styling

--- logic.js
+++ logic.js
@@ -1,1 +1,31 @@
+$(document).ready(function() {
+    let team1Name = "";
+    let team2Name = "";
+    let question = 1;
+    $("#nextQuestion").hide();
 
+    $("#beginGame").click(function() {
+        team1Name = $("#team1Entry").val().trim();
+        team2Name = $("#team2Entry").val().trim();
+        $(".teamButtons").hide();
+        $("#beginGame").hide();
+        $("#team1").html(team1Name);
+        $("#team2").html(team2Name);
+        $("#questionNumber").html("Question Number " + question);
+
+        let coinFlip = Math.floor((Math.random() * 2) + 1);
+        
+        if (coinFlip === 1) {
+            $("#questionArea").html(team1Name + " will go first");
+            $("#one").css("background-color", "red");
+        } else {
+            $("#questionArea").html(team2Name + " will go first");
+            $("#two").css("background-color", "green");
+        }
+        $("#nextQuestion").fadeIn("slow");
+    });
+
+
+
+
+});
