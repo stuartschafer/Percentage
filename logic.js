0e4ee64841521234b04fca06175b432f03b00f21
Stuart Schafer <stuart_schafer@hotmail.com>
2017-11-03 19:11:59 -0400

    Added a timer and question display :japanese_ogre:

--- logic.js
+++ logic.js
@@ -2,7 +2,17 @@ $(document).ready(function() {
     let team1Name = "";
     let team2Name = "";
     let question = 1;
+    let x = 0;
+    let time = 300;
     $("#nextQuestion").hide();
+    let questions = ["What percent of American homes have some sort of computer?",
+    "Women earn what % of all undergraduate computer and information sciences degrees?",
+    "% of Fortune 50 companies that use GITHUB Enterprise",
+    "This % of programmer working time is spent surfing the source code. This navigation involves research, observation, information gathering and other activities.",
+    "Percentage of bugs that can be discovered (but not necessarily fixed) when more than one person reviews the source code.",
+    "In 2017 Q3, Javascript had the highest pull % on GitHub.  What was it?",
+    "This percentage of the world’s currency is physical money, the rest only exists on computers."];
+    let answers = [85, 18, 52, 30, 60, 22, 8];
 
     $("#beginGame").click(function() {
         team1Name = $("#team1Entry").val().trim();
@@ -25,7 +35,28 @@ $(document).ready(function() {
         $("#nextQuestion").fadeIn("slow");
     });
 
+    $("#nextQuestion").click(function() {
+         $("#questionArea").html(questions[3]);
+         $("#nextQuestion").hide();
+         timer();
+    });
 
 
+    function timer() {
+	    intervalId = setInterval(count, 10);
+    }
+
+    function count () {
+        if (time === 0) {
+            clearInterval(intervalId);
+            $("#timer").css("font-size", "90px");
+            $("#timer").html("TIME TO MAKE A GUESS");
+        } else if (time % 100 === 0) { 
+            if (time < 1100) {
+                $("#timer").css("color", "red");
+            }
+            $("#timer").html(time/100); }
+            time--;
+    }
 
 });
